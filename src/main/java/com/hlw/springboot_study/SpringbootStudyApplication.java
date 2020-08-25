0fc997bbf13e266c418603936c0647b37c23c898
houlongwang <houlongwang@51jk.com>
2020-08-25 18:51:14 +0800

    feat:    1.优雅调用SpringMvc请求

--- src/main/java/com/hlw/springboot_study/SpringbootStudyApplication.java
+++ src/main/java/com/hlw/springboot_study/SpringbootStudyApplication.java
@@ -1,15 +1,42 @@
 package com.hlw.springboot_study;
 
+import com.hlw.springboot_study.service.bo.Quote;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
+@Slf4j
 public class SpringbootStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootStudyApplication.class, args);
     }
-
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory){
+        return new RestTemplate(factory);
+    }
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+            assert quote != null;
+            log.info(quote.toString());
+        };
+    }
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(15000);
+        factory.setReadTimeout(5000);
+        return factory;
+    }
 }
