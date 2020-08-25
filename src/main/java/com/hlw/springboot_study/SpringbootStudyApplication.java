be5f7d6a2b90cb4957febe738a2c554adbdc6daa
houlongwang <houlongwang@51jk.com>
2020-08-25 18:38:57 +0800

    feat:    1.添加定时任务部署

--- src/main/java/com/hlw/springboot_study/SpringbootStudyApplication.java
+++ src/main/java/com/hlw/springboot_study/SpringbootStudyApplication.java
@@ -2,8 +2,10 @@ package com.hlw.springboot_study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringbootStudyApplication {
 
     public static void main(String[] args) {
